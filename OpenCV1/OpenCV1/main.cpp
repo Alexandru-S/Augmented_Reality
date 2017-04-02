da52fb9796463fb028f465f67def94b81c8de8d7
Alexandru-S <suleaa@tcd.ie>
2017-04-02 03:23:09 +0100

    ädded webcam

--- OpenCV1/OpenCV1/main.cpp
+++ OpenCV1/OpenCV1/main.cpp
@@ -1,16 +1,18 @@
 /**
-* @file Morphology_1.cpp
-* @brief Erosion and Dilation sample code
-* @author OpenCV team
+* @OpenCV Project
+* @Splicing
+* @author Alexandru Sulea
 */
 
 #include <opencv2/core/core.hpp>
+#include "opencv2/core/utility.hpp"
 #include "opencv2/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/flann.hpp"
+#include "opencv2/text.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "Img.h"
 
@@ -20,10 +22,8 @@
 #include <stdio.h>
 #include <iostream>
 
-
 #include <iostream>
 #include <algorithm>
-
 
 using namespace cv;
 using namespace cv::xfeatures2d;
@@ -32,31 +32,75 @@ using namespace std;
 int inp;
 
 void printSeparator();
-void Video();
+int Video();
+int Webcam();
 
 int main()
 {
 	int inp1;
 	printSeparator();
-	printf("please select 1 for video and 2 for img stitching\n");
+	printf("--- AUGMENTED REALITY REPORT ---\n");
+	printSeparator();
+	printf("please select from the following:\n 1 for video \n 2 for img \n 3 for webcam \n");
 	cin >> inp1;
 
 	if(inp1==1)
 	{ 
-	
+		Video();
 	}
 	else if (inp1 == 2)
 	{
 		Img();
 	}
-
+	else if (inp1 == 3)
+	{
+		Webcam();
+	}
                                      
 	return 0;
 }
 
-void Video() 
+int Video() 
 {
-	
+	VideoCapture video;
+	if (!video.open(0))
+	{
+		return 0;
+	}
+
+	for (;;)
+	{
+		Mat frame;
+		video >> frame;
+		if (frame.empty()) break; // end of video stream
+		imshow("this is you, smile! :)", frame);
+		if (waitKey(10) == 27) break; // stop capturing by pressing ESC 
+	}
+	// the camera will be closed automatically upon exit
+	// cap.close();
+	return 0;
+}
+
+
+//http://answers.opencv.org/question/1/how-can-i-get-frames-from-my-webcam/
+//to open webcaam
+int Webcam()
+{
+	VideoCapture webcam;
+	if (!webcam.open(0))
+	{
+		return 0;
+	}
+
+	for (;;)
+	{
+		Mat frame;
+		webcam >> frame;
+		if (frame.empty()) break; // end of video stream
+		imshow("webcam", frame);
+		if (waitKey(10) == 27) break; // stop capturing by pressing ESC 
+	}
+	return 0;
 }
 
 void printSeparator()
