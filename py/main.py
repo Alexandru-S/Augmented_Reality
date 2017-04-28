6c2b2bc508a60e1d2d050bb597dd301146225d53
Alexandru-S <suleaa@tcd.ie>
2017-04-28 05:07:36 +0100

    almost have video panorama stitching

--- py/main.py
+++ py/main.py
@@ -12,14 +12,16 @@ import os
 import sys
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
+from PIL import Image
+
 #from matchers import matchers
 
 firstbool = True
 
 cap = cv2.VideoCapture(0)
 
-imgL = cv2.imread('im1.jpg', 0)
-imgR = cv2.imread('im2.jpg', 0)
+imgL = cv2.imread('p1.jpg', 0)
+imgR = cv2.imread('p2.jpg', 0)
 
 Fimg = cv2.imread('img2.jpg', 0)
 Simg = cv2.imread('img1.jpg', 0)
@@ -66,7 +68,7 @@ if choise is '1':
 			#notnot = cv2.bitwise_not(fgmask)
 
 			#res = cv2.bitwise_and(frame,frame,mask = fgmask)
-			cv2.imshow('frame', frame)
+		#	cv2.imshow('frame', frame)
 
 elif choise is '2':
 	print( "option 2" )
@@ -78,30 +80,85 @@ elif choise is '2':
 
 
 elif choise is '3':
+
+	
 	print( "option 3" )
 	count = 0
 	seconds = 2
-	cap = cv2.VideoCapture('video2.mp4')
+	cap = cv2.VideoCapture('video1.mp4',0)
 	fps = cap.get(cv2.CAP_PROP_FPS) 
 	multiplier = fps * seconds
 	prev =0
+	prev_des = 0
+	prev_kp = 0
+
+
+
+	ratio = 0.75
+	reprojThresh = 4.0
+
+
+	sift = cv2.xfeatures2d.SIFT_create()
+	#http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_matcher/py_matcher.html
 	while(cap.isOpened()):
 		
 		frameId = int(round(cap.get(1)))
 		ret, frame = cap.read()
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
 
 		if frameId % multiplier == 0:
-			cv2.imshow('frame3', prev)
-			cv2.imshow('frame',frame)
-			time.sleep(.001)
+
+			gray_frame = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY );
+			height, width = frame.shape[:2]
+			print("width:"+ str(width) + " height:" + str(height) )
+
+			kp, des = sift.detectAndCompute(gray_frame, None)
+
+			FLANN_INDEX_KDTREE = 0
+			index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+			search_params = dict(checks=50)  # or pass empty dictionary
+
+			#cv2.drawKeypoints(gray_frame,kp,frame,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS )
+
+
+			flann = cv2.FlannBasedMatcher(index_params, search_params)
+			if (count>2):
+				matches = flann.knnMatch(prev_des, des, k=2)
+
+				matchesMask = [[0, 0] for i in range(len(matches))]
+				for i, (m, n) in enumerate(matches):
+					if m.distance < 0.7 * n.distance:
+						matchesMask[i] = [1, 0]
+
+				draw_params = dict(matchColor=(0, 255, 0),
+								   singlePointColor=(255, 0, 0),
+								   matchesMask=matchesMask,
+								   flags=0)
+
+				img3 = cv2.drawMatchesKnn(prev, prev_kp, frame, kp, matches, None, **draw_params)
+
+				dst = np.array([[0, 0],[width - 1, 0],[width - 1, height - 1],[0, height - 1]], dtype="float32")
+				rect = np.zeros((4, 2), dtype="float32")
+				M = cv2.getPerspectiveTransform(rect, dst)
+				warp = cv2.warpPerspective(img3, M, (width*count, height))
+				#cv2.warp
+				#cv2.imshow('newframe', frame)
+				#cv2.imshow('prev', prev)
+				img3 = cv2.resize(img3, (0, 0), fx=0.5, fy=0.5)
+				cv2.imshow('img3',img3 )
+				warp = cv2.resize(warp, (0, 0), fx=0.25, fy=0.25)
+				cv2.imshow('warp', warp)
+				#cv2.show()
+
+
+
+
+			#time.sleep(.001)
 			prev=frame
+			prev_kp = kp
+			prev_des = des
+			count = count + 1
 
-		
-
-
-
-		count = count + 1
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
@@ -113,6 +170,7 @@ elif choise is '3':
 elif choise is '4':
 	print("option 4")
 
+	
 
 
 stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
