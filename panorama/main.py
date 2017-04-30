4a15d8c2bd757b604b835f39808f9b0751c5f50f
Alexandru-S <suleaa@tcd.ie>
2017-04-30 05:09:05 +0100

    added flann based matcher

---
+++ panorama/main.py
@@ -0,0 +1,85 @@
+from pyimagesearch.panorama import Stitcher
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+from PIL import Image
+
+ap = argparse.ArgumentParser()
+
+choice =0
+
+print('please choose from the options')
+choice = input('1 : basic image stitching\n2 : basic panorama stitching \n\n')
+
+if choice is '1':
+    print('option 1 selected')
+
+    imageA = cv2.imread('images/bryce_left_01.png')
+    imageB = cv2.imread('images/bryce_right_01.png')
+    imageA = cv2.resize(imageA, (0, 0), fx=0.5, fy=0.5)
+    imageB = cv2.resize(imageB, (0, 0), fx=0.5, fy=0.5)
+
+    stitcher = Stitcher()
+    (result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+
+    cv2.imshow("Keypoint Matches", vis)
+    cv2.imshow("Result", result)
+
+if choice is '2':
+    count = 1
+    seconds = 5
+    prev= 0
+    prev_result = 0
+    cap = cv2.VideoCapture('video2.mp4', 0)
+    capr = cv2.VideoCapture('toright.mp4', 0)
+    capl = cv2.VideoCapture('toleft.mp4', 0)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    multiplier = fps * seconds
+
+    print('option 2 selected')
+    cap = cv2.VideoCapture('video2.mp4', 0)
+
+    imageA = cv2.imread('images/bryce_left_01.png')
+    imageB = cv2.imread('images/bryce_right_01.png')
+    imageA = cv2.resize(imageA, (0, 0), fx=0.5, fy=0.5)
+    imageB = cv2.resize(imageB, (0, 0), fx=0.5, fy=0.5)
+
+
+    while (cap.isOpened()):
+        key = cv2.waitKey(1) & 0xff
+        frameId = int(round(cap.get(1)))
+        ret, frame = cap.read()
+
+
+
+
+        if frameId % multiplier == 0:
+            if count > 2:
+                frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                #cv2.imshow('frame', prev)
+                #cv2.imshow('prev', frame)
+                stitcher = Stitcher()
+                (result, vis) = stitcher.stitch([frame, prev], showMatches=True)
+                cv2.imshow('result', result)
+                cv2.imshow('vis', vis)
+
+                #(result2, vis2) = stitcher.stitch([prev_result, frame], showMatches=True)
+
+                #cv2.imshow('result2', result2)
+                #cv2.imshow('vis2', vis2)
+                prev_result= result
+
+
+            prev=frame
+
+            count += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    print('complete')
+cv2.waitKey(0)
